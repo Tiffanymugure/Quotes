81a512d12d0ab2b0dc9a5ff317eb6db95278b931
tiffany mugure <amiratiffany@gmail.com>
2018-10-21 01:40:04 +0300

    buttons and forms added to files

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -9,7 +9,7 @@ import {Quote} from '../quotes'
 })
 export class QuoteComponent implements OnInit {
  quotes =[
-   new Quote(1,"Happiness quite unshared can scarcely be called happiness'then it has no taste.","Charlotte Bronte","Aira",0,0,new Date(2018,10,17)),
+   new Quote(1,"Happiness quite unshared can scarcely be called happiness'then it has no taste.","Charlotte Bronte","Amira",0,0,new Date(2018,10,17)),
    new Quote(2,"The smarter you get the less you talk.","Curtis James","Ashy Wanjiru",0,0,new Date(2018,10,17)),
    new Quote(2,"If we have no peace,it is because we have forgotten we belong to each other.","Mother Theresa","Hamida",0,0,new Date(2018,10,17)),
        ];
