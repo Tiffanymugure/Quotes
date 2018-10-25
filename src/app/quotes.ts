208346f3fe99cabdeff83d63b5535155ffa24f33
tiffany mugure <amiratiffany@gmail.com>
2018-10-25 09:32:14 +0300

    Auto-generated commit

--- src/app/quotes.ts
+++
@@ -1,9 +0,0 @@
-export class Quote {
-  public showAuthor:boolean;
-  public showFan:boolean;
-  constructor(public id:number, public name:string, public author:string, public fan:string, public upvotes:number, public downvotes:number, public notedDate:Date){
-    this.showAuthor=false;
-    this.showFan=false;
-  }
-
-}
